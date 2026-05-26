83975d4dc02379567b42e97dae3eb116e7584d28
Hoveringthyme76 <138344757+Hoveringthyme76@users.noreply.github.com>
2026-05-26 09:15:49 -0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+     I'm thinking about making this project test a users ability to come up with multiple project ideas. I'm going to make 100 ideas as the max limit.
+     It's possible that I also have to use the getline() function to account for any spaces the user tries to input.
+     As for how the program tells you how many ideas you can come up with, I might have to look into it further. Maybe use an array, or some different data structure?
+
+     CHALLENGE MODE:
+     rules to challenge mode, I will attempt to put limitations on myself so I can see the quality of the program that much better, even with certain constraints being applied.
+
+     For this project, I will have to not include any if else conditionals. Which means only tenary operators or switches are allowed when applying logic to this program.
+*/
+int main()
+{
+
+
+     return 0;
+}
